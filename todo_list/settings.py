c7819912078a5943f597053e6bae216a2bc2e7c7
ivan <ivanbelousov14@gmail.com>
2022-09-30 11:20:23 +0300

    create model user

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -30,9 +30,9 @@ environ.Env.read_env(os.path.join(BASE_DIR, '.env'))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = env('SECRET_KEY')
-
-# SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env('DEBUG')
+# SECURITY WARNING: don't run with debug turned on in production!
+
 
 ALLOWED_HOSTS = ["*"]
 
@@ -45,7 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'todo_list'
+    'todo_list',
+    'core',
 ]
 
 MIDDLEWARE = [
