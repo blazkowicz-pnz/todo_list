2ecda7541440460e98d772f717ad63c4e25b699c
Техническая Поддержка <serviceman@hospital-6.local>
2022-11-02 09:29:46 +0300

    board create-migrations

--- goals/models.py
+++ goals/models.py
@@ -3,6 +3,7 @@ from django.db import models
 from core.models import User
 
 
+
 class BaseModel(models.Model):
     created = models.DateTimeField(
         verbose_name="Дата создания",
@@ -15,6 +16,47 @@ class BaseModel(models.Model):
 
     class Meta:
         abstract = True
+
+
+
+class Board(BaseModel):
+    class Meta:
+        verbose_name = "Доска"
+        verbose_name_plural = "Доски"
+
+    title = models.CharField(verbose_name="Название", max_length=255)
+    is_deleted = models.BooleanField(verbose_name="Удалена", default=False)
+
+
+
+
+class BoardParticipant(BaseModel):
+    class Meta:
+        unique_together = ("board", "user")
+        verbose_name = "Участник"
+        verbose_name_plural = "Участники"
+
+    class Role(models.IntegerChoices):
+        owner = 1, "Владелец"
+        writer = 2, "Редактор"
+        reader = 3, "Читатель"
+
+    board = models.ForeignKey(
+        Board,
+        verbose_name="Доска",
+        on_delete=models.PROTECT,
+        related_name="participants",
+    )
+    user = models.ForeignKey(
+        User,
+        verbose_name="Пользователь",
+        on_delete=models.PROTECT,
+        related_name="participants",
+    )
+    role = models.PositiveSmallIntegerField(
+        verbose_name="Роль", choices=Role.choices, default=Role.owner
+    )
+
 
 
 class GoalCategory(BaseModel):
@@ -35,6 +77,14 @@ class GoalCategory(BaseModel):
     is_deleted = models.BooleanField(
         verbose_name="Удалена",
         default=False)
+
+    board = models.ForeignKey(
+        Board,
+        verbose_name="Доска",
+        on_delete=models.PROTECT,
+        related_name="categories"
+
+    )
 
     def __str__(self):
         return self.title
