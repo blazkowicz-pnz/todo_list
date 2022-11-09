1020903d2dcc849b49db93f4b7fea68aaef90e76
blazkowicz-pnz <ivanbelousov14@gmail.com>
2022-11-09 21:06:38 +0300

    echo bot1

--- bot/management/commands/runbot.py
+++ bot/management/commands/runbot.py
@@ -1,10 +1,27 @@
+from bot.tg.client import TgClient
+from django.core.management import BaseCommand
 
+from todo_list import settings
+from bot.models import TgUser
+from bot.tg.models import Message
 
 
 class Command(BaseCommand):
     def __init__(self, *args, **kwargs):
         super().__init__(*args, **kwargs)
-        self.tg_client = TgClient(settings.BOT_TOKEN)
+        self.tg_client = TgClient(settings.BOT_TOKEN)a
+
+    def handle_message(self, msg: Message):
+        tg_user, _ = TgUser.objects.select_related("user").get_or_create(
+            chat_id=msg.chat.id,
+            defaults={
+                "username": msg.from_.username
+            }
+        )
+        if tg_user.user:
+            self.handle_verified_user(msg=msg, tg_user=tg_user)
+        else:
+            self.handle_unverified_user(msg=msg, tg_user=tg_user)
 
     def handle(self, *args, **options):
         offset = 0
@@ -13,4 +30,6 @@ class Command(BaseCommand):
             for item in res.result:
                 offset = item.update_id + 1
                 self.tg_client.send_message(chat_id=item.message.chat.id, text=item.message.text)
+
+
 
--- bot/tg/models.py
+++ bot/tg/models.py
@@ -1,27 +1,27 @@
 from pydantic import BaseModel, Field
-
+from typing import Optional
 
 class MessageFrom(BaseModel):
     id: int
     first_name: str
-    last_name: str or None = None
-    username: str
+    last_name: Optional[str] = None
+    username: Optional[str] = None
 
 
 class Chat(BaseModel):
     id: int
     type: str
-    first_name: str or None = None
-    last_name: str or None = None
-    username: str or None = None
-    title: str or None = None
+    first_name: Optional[str] = None
+    last_name: Optional[str] = None
+    username: Optional[str] = None
+    title: Optional[str] = None
 
 
 class Message(BaseModel):
     message_id: int
     from_: MessageFrom = Field(..., alias="from")
     chat: Chat
-    text: str or None = None
+    text: Optional[str] = None
 
     class Config:
         allow_population_by_field_name = True
@@ -29,12 +29,12 @@ class Message(BaseModel):
 
 class UpdateObj(BaseModel):
     update_id: int
-    message = Message
+    message: Message
 
 
-class GetUpdateResponse(BaseModel):
+class GetUpdatesResponse(BaseModel):
     ok: bool
-    result:  list[UpdateObj] = []
+    result: list[UpdateObj] = []
 
 
 class SendMessageResponse(BaseModel):
