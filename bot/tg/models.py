7ac574a56eb525e811332f860a58d1aa034e5814
blazkowicz-pnz <ivanbelousov14@gmail.com>
2022-11-09 20:11:25 +0300

    echo bot

---
+++ bot/tg/models.py
@@ -0,0 +1,43 @@
+from pydantic import BaseModel, Field
+
+
+class MessageFrom(BaseModel):
+    id: int
+    first_name: str
+    last_name: str or None = None
+    username: str
+
+
+class Chat(BaseModel):
+    id: int
+    type: str
+    first_name: str or None = None
+    last_name: str or None = None
+    username: str or None = None
+    title: str or None = None
+
+
+class Message(BaseModel):
+    message_id: int
+    from_: MessageFrom = Field(..., alias="from")
+    chat: Chat
+    text: str or None = None
+
+    class Config:
+        allow_population_by_field_name = True
+
+
+class UpdateObj(BaseModel):
+    update_id: int
+    message = Message
+
+
+class GetUpdateResponse(BaseModel):
+    ok: bool
+    result:  list[UpdateObj] = []
+
+
+class SendMessageResponse(BaseModel):
+    ok: bool
+    result: Message
+
