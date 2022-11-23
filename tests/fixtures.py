85162c7d051c4b5ce836866b501f23e24dc1817d
blazkowicz-pnz <ivanbelousov14@gmail.com>
2022-11-23 19:12:34 +0300

    diploma v12

--- tests/fixtures.py
+++ tests/fixtures.py
@@ -5,7 +5,7 @@ import pytest
 @pytest.fixture
 @pytest.mark.django_db
 def get_credentials(client, user):
-    """Получение токена для аутентификации пользователя"""
+    """get token for authentication"""
     password = user.password
 
     user.set_password(password)
@@ -14,23 +14,3 @@ def get_credentials(client, user):
     token = base64.b64encode(f'{user.username}:{password}'.encode()).decode()
 
     return 'Basic ' + token
-
-# import pytest
-#
-#
-# @pytest.fixture
-# @pytest.mark.django_db
-# def my_token(client, django_user_model):
-#     username = "test3"
-#     password = "t1r2i3g4"
-#
-#     django_user_model.objects.create_user(
-#         username=username,
-#         password=password
-#     )
-#     response = client.post(
-#         "/core/login",
-#         {"username": username, "password": password},
-#         format="json"
-#     )
-#     return response.data.cookies["csrftoken"]
